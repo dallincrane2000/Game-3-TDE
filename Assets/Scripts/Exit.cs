040aaf51d068a431804a3549a63d09051692b54c
dallincrane2000 <dallincrane@hotmail.com>
2022-12-10 17:57:55 -0800

    Made a Murder Door Prefab

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -7,13 +7,24 @@ public class Exit : MonoBehaviour
     public GameObject doorClosed;
     public GameObject doorOpen;
 
+    public List<GameObject> enemies;
+
     void Update()
     {
-        if (GameObject.FindWithTag("Enemy") == false)
+        // iterates through all of the enemies and checks if they are "alive" if they are not they get removed from the list
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (!(enemies[i].activeInHierarchy))
+            {
+                enemies.RemoveAt(i);
+            }
+        }
+        // checks if list is empty and it is empty the door opens
+        if(enemies.Count == 0)
         {
             doorClosed.SetActive(false);
             doorOpen.SetActive(true);
         }
-    }
 
+    }
 }
