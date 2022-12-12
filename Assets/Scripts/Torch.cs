fb9e982b1239fa65f08b0de72e5349d7f9a1a9b6
dallincrane2000 <dallincrane@hotmail.com>
2022-12-11 19:56:46 -0800

    Torch Flicker

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -1,74 +1,32 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Rendering.Universal;
 
-// Written by Steve Streeting 2017
-// License: CC0 Public Domain http://creativecommons.org/publicdomain/zero/1.0/
-
-/// <summary>
-/// Component which will flicker a linked light while active by changing its
-/// intensity between the min and max values given. The flickering can be
-/// sharp or smoothed depending on the value of the smoothing parameter.
-///
-/// Just activate / deactivate this component as usual to pause / resume flicker
-/// </summary>
 
 public class Torch : MonoBehaviour
 {
-    [Tooltip("External light to flicker; you can leave this null if you attach script to a light")]
-    public new Light light;
-    [Tooltip("Minimum random light intensity")]
-    public float minIntensity = 0f;
-    [Tooltip("Maximum random light intensity")]
-    public float maxIntensity = 1f;
-    [Tooltip("How much to smooth out the randomness; lower values = sparks, higher = lantern")]
-    [Range(1, 50)]
-    public int smoothing = 5;
+    public float flickerSpeed = 0.5f;
+    public float flickerAmount = 0.1f;
 
-    // Continuous average calculation via FIFO queue
-    // Saves us iterating every time we update, we just change by the delta
-    Queue<float> smoothQueue;
-    float lastSum = 0;
-
-
-    /// <summary>
-    /// Reset the randomness and start again. You usually don't need to call
-    /// this, deactivating/reactivating is usually fine but if you want a strict
-    /// restart you can do.
-    /// </summary>
-    public void Reset()
-    {
-        smoothQueue.Clear();
-        lastSum = 0;
-    }
+    private Light2D light;
+    private float baseIntensity;
 
     void Start()
     {
-        smoothQueue = new Queue<float>(smoothing);
-        // External or internal light?
-        if (light == null)
-        {
-            light = GetComponent<Light>();
-        }
+        // Get the light component
+        light = GetComponent<Light2D>();
+
+        // Store the base intensity of the light
+        baseIntensity = light.intensity;
     }
 
     void Update()
     {
-        if (light == null)
-            return;
+        // Generate a random noise value using Perlin noise
+        float noise = Mathf.PerlinNoise(Time.time * flickerSpeed, 0);
 
-        // pop off an item if too big
-        while (smoothQueue.Count >= smoothing)
-        {
-            lastSum -= smoothQueue.Dequeue();
-        }
-
-        // Generate random new item, calculate new average
-        float newVal = Random.Range(minIntensity, maxIntensity);
-        smoothQueue.Enqueue(newVal);
-        lastSum += newVal;
-
-        // Calculate new smoothed average
-        light.intensity = lastSum / (float)smoothQueue.Count;
+        // Use the noise value to control the intensity of the light
+        light.intensity = baseIntensity + noise * flickerAmount;
     }
 }
 
