93124ddf0332ee78db82651774f07f8d2f4cfe79
DuckieOnQuacks <jojopaschke@gmail.com>
2022-12-12 13:58:55 -0800

    Moved green divine soul to the portal arena and it spawns after all enemies have died

--- Assets/Scripts/spawnDivineSoul.cs
+++ Assets/Scripts/spawnDivineSoul.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class spawnDivineSoul : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject soul;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameObject.FindWithTag("Enemy") == false)
+        {
+            Instantiate(soul, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
